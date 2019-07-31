d986077cccb63e7c1f1f3247e44e17a60a11303c
ehmatthes <eric@example.com>
2019-07-31 16:28:28 +0800

    add mock proxy

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>
+      {{ msg }}
+    </h2>
+    <div v-for="(item, index) in testData" :key="index">
+      {{ item }}
+    </div>
   </div>
 </template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "about",
+  data() {
+    return {
+      testData: [],
+      msg: "helloWorld"
+    };
+  },
+  mounted() {
+    axios
+      .get("api/seller")
+      .then(res => {
+        this.testData = res.data.infos;
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  }
+};
+</script>
